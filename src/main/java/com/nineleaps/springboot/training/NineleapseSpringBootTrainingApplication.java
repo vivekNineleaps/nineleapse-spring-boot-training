657950ab3a3dc692241ccb86413a395e20c1ed35
streamvk <vivekrathi74@gmail.com>
2020-05-26 23:03:19 +0530

    First Commit

---
+++ src/main/java/com/nineleaps/springboot/training/NineleapseSpringBootTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.nineleaps.springboot.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NineleapseSpringBootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NineleapseSpringBootTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nineleaps/springboot/training/NineleapseSpringBootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nineleaps.springboot.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NineleapseSpringBootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
